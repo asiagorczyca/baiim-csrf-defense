0cc70be724da43d47e67726c7880ebd125a2dbf9
Joanna Gorczyca <jgorczyca@student.agh.edu.pl>
2026-01-15 14:03:29 +0100

    test

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(session(sessionConfig = {
     saveUninitialized: true,
     cookie: { 
         httpOnly: true,
-        // sameSite: 'Lax',  <-- brak SameSite oznacza, że zadanie 1 nie jest wykonane
     }
 }
 ));
@@ -54,38 +53,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/update-email', (req, res) => {
-    const failures = [];
-
-
-    const origin = req.get('Origin');
-    const validOrigin = 'http://localhost:3000';
-    const originOK = origin === validOrigin;
-    if (!originOK) failures.push(3);
-
-    const contentType = req.get('Content-Type') || '';
-    const ctOK = contentType.startsWith('application/json');
-    if (!ctOK) failures.push(4);
-
-
-    const serverToken = req.session?.csrfToken;
-    const clientToken = req.body?.csrfToken;
-    const csrfOK = serverToken && clientToken && serverToken === clientToken;
-    if (!csrfOK) failures.push(2);
-
-
-    if (!sessionConfig.cookie.sameSite) failures.push(1);
-
-    if (failures.length > 0) {
-        console.log("Niepoprawnie wykonane zadania:", failures);
-    } else {
-        console.log("Wszystkie zabezpieczenia działają poprawnie!");
-    }
-
-
-    if (origin && failures.length === 0) {
-        userDB.email = req.body.email;
-        return res.send("Zmiana zatwierdzona");
-    }
+ 
 
     userDB.email = req.body.email;
     res.send("Zmiana zatwierdzona");
